63b3db75e3fea9c47e994cce289aa02a9abe7808
Peter <peter_8787@outlook.com>
2022-11-01 15:06:05 +0300

    решена Задача2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
 2, 4 -> 16
 
 */
-
+/*
 double Number (double a, double b)
 {
     return Math.Pow (a , b);
@@ -16,3 +16,32 @@ Console.WriteLine("Input b");
 double b = Convert.ToInt32(Console.ReadLine());
 double result = Number(a, b);
 Console.WriteLine($" Ответ = {result}");
+*/
+
+/*Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+
+*/
+
+int FindSum(int num)
+{   
+    int size = Convert.ToString(num).Length;
+    int ed = 0;
+    int findSum = 0;
+    for(int i = 0; i < size; i++)
+    {   
+        ed = num - num % 10;
+        findSum = findSum + (num - ed);
+        num = num / 10;
+    }
+    return findSum;
+}
+Console.WriteLine("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int findSum = FindSum(num);
+Console.WriteLine($"сумма цифр в числе = {findSum}");
